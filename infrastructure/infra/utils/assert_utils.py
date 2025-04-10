8fa1705ce91bc22a9a7250ac46313f54c33e8fac
lior <lior277@gmail.com>
2025-04-10 11:01:19 +0300

    fix

--- infrastructure/infra/utils/assert_utils.py
+++ infrastructure/infra/utils/assert_utils.py
@@ -1,19 +1,13 @@
 class CustomAssert:
     @staticmethod
     def assert_all(*assertions):
-        """
-        Executes all assertions passed to it and collects any failures.
-
-        :param assertions: Lambda functions containing assertions
-        """
         errors = []
 
         for i, assertion in enumerate(assertions):
             try:
-                assertion()  # This calls each lambda (which contains the assertion)
+                assertion()
             except AssertionError as e:
                 errors.append(f"Assertion {i + 1} failed: {str(e)}")
 
-        # If there are any errors, raise them all together with detailed information
         if errors:
             raise AssertionError("\n".join(errors))
